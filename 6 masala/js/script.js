0256176545ced7e5612212574ff6e69a2b86226c
Shermatov Davron Devloper full stack <93197110+DavronShermatov@users.noreply.github.com>
2022-10-07 11:02:24 +0300

    6 masala

--- 6 masala/js/script.js
+++ 6 masala/js/script.js
@@ -1,1 +1,17 @@
 'use strict'
+
+// 6 masala
+const date = +prompt('yilni kiriting','')
+function year(date) {
+    if(date%4 ===0){
+        console.log(`12/09/${date}`)
+        const textDate = document.querySelector('#demo')
+        textDate.innerHTML = `<h1>12/09/${date}</h1>`
+    }else{
+        console.log(`13/09/${date}`) 
+        const textDate = document.querySelector('#demo')
+        textDate.innerHTML = `<h1>13/09/${date}</h1>`
+    }
+}
+
+year(date)
