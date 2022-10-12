d15bee6116638f2385086ad674372d3586b8d475
Shermatov Davron Devloper full stack <93197110+DavronShermatov@users.noreply.github.com>
2022-10-12 19:15:42 +0300

    11 masala

--- 9 masala/js/script.js
+++ 9 masala/js/script.js
@@ -1,7 +1,14 @@
 'use strict'
 const arrNumber = prompt("Berilgan sonlarni kiriting va ular orasida , belgisi bo'lsin", ""),
     arr = arrNumber.split(",")
-let  n = arr.length
 
+
+for(let i=0; i<=arr.length; i++){
+    for(let n=0; n<=arr.length; i++){
+        if(arr[i]==arr[n]){
+            arr.splice(i,1)
+        }
+    }
+}
 
 console.log(arr)
